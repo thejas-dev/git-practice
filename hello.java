948388684bc39fbdf6d42ee23929046d0aa4bcc8
Thejas Hari <thejas.hari@ei.study>
2025-01-11 16:45:34 +0530

    added hello world
    makeing branch diverge
    
    created java
    
    created java 2
    
    created java 3
    
    changes
    
    commit1
    
    commit2
    
    commit2

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World2!");
+        System.out.println("Hello, World3!");
+        String text = "hello world";
+    }
+}
---
+++ hello.py
@@ -0,0 +1,7 @@
+print('hello world')
+a = 'hello world'
+print("a : " + a)
+b = 'beta world'
+print("b : " + b)
+c = 'alpha world'
+print("c : " + c)
